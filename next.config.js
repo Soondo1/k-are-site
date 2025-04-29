502eab3afe46482c43bfbc15a84b17840cc4729c
hayyoung <vanilahay@gmail.com>
2025-04-30 04:32:34 +0900

    updated next.config.js to support static files

--- next.config.js
+++ next.config.js
@@ -1,14 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-    },
-    webpack: (
-      config,
-      { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-    ) => {
-      return config
-    },
-  }
+  output: 'export',  // Enables static HTML export
+  images: {
+    unoptimized: true,  // Required for static export
+    domains: [
+      'cdn.sanity.io',
+      'source.unsplash.com',
+      'images.unsplash.com'
+    ],
+  },
+  // Required if you deploy to GitHub Pages as a project site (username.github.io/repo-name)
+  basePath: process.env.NODE_ENV === 'production' ? '/k-are' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/k-are' : '',
   
-  module.exports = nextConfig
+  // Other configurations...
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
